bacb4aeeb1f21e697369dfcd02536793cb9bd4f5
Kaique <kaique@neurony.com.br>
2022-03-30 20:45:52 -0300

    Adiciona tooltip em contatos - header

--- kk/src/components/header/Header.jsx
+++ kk/src/components/header/Header.jsx
@@ -1,15 +1,32 @@
 import React from 'react';
 import './Header.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faLinkedinIn, faInstagram, faArtstation, faWhatsapp } from '@fortawesome/free-brands-svg-icons'
 
 export default (props) => {
-    return(
+    return (
         <section className='header'>
             <div className='logo'><a>KK</a></div>
             <div className='navigation'>
                 <a className='modelos'>Modelos</a>
                 <a className='texturas'>Texturas</a>
                 <a className='projetos'>Projetos</a>
-                <a className='contato'>Contato</a>
+                <a className='contato'>Contato
+                    <div className='contato-tooltip'>
+                        <a href="https://www.linkedin.com/in/kaique-ferreira-854a75139/" target="_blank"><FontAwesomeIcon icon={faLinkedinIn} />
+                            Linkedin
+                        </a>
+                        <a href="https://www.instagram.com/kkaique.ferreira/" target="_blank"><FontAwesomeIcon icon={faInstagram} />
+                            Instagram
+                        </a>
+                        <a href="https://www.artstation.com/kaiqueferreira" target="_blank"><FontAwesomeIcon icon={faArtstation} />
+                            Artstation
+                        </a>
+                        <a href="https://api.whatsapp.com/send?phone=+55 27 99510-5454" target="_blank"><FontAwesomeIcon icon={faWhatsapp} />
+                            WhatsApp
+                        </a>
+                    </div>
+                </a>
             </div>
         </section>
     )
