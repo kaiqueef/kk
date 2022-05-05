2653b2de0d35976db250cee67e140305b0f229bd
Kaique <kaique@neurony.com.br>
2022-05-05 08:03:23 -0300

    Separa as rotas

--- kk/src/App.js
+++ kk/src/App.js
@@ -7,26 +7,14 @@ import Footer from './components/footer/Footer'
 import NotFound from './Pages/NotFound';
 import Home from './Pages/Home'
 import Portfolio from './Pages/Portfolio/Portfolio';
+import RoutesApp from './routes';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
       </header>
-        <BrowserRouter>
-        <Header />
-          <Routes>
-            <Route path="*" element={<NotFound />} />
-            <Route path="/" element={<Home />} />
-            <Route path="/portfolio" element={<Portfolio />}>
-              <Route path='all' />
-              <Route path='ArchViz' />
-              <Route path='3d' />
-              <Route path='grafite' />
-            </Route>
-            </Routes>
-          <Footer />
-        </BrowserRouter>
+          <RoutesApp/>
     </div>
   );
 }
---
+++ kk/src/routes.js
@@ -0,0 +1,32 @@
+//TODO:: CRIAR ARQUIVO COM TODAS AS ROTAS E CHAMA-LO NO APP
+
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+import Header from './components/header/Header';
+import Footer from './components/footer/Footer';
+
+import NotFound from './Pages/NotFound';
+import Home from './Pages/Home';
+import Portfolio from './Pages/Portfolio/Portfolio';
+
+function RoutesApp() {
+    return (
+        <BrowserRouter>
+            <Header />
+            <Routes>
+                <Route path="*" element={<NotFound />} />
+                <Route path="/" element={<Home />} />
+                <Route path="/portfolio" element={<Portfolio />}>
+                    <Route path='all' />
+                    <Route path='ArchViz' />
+                    <Route path='3d' />
+                    <Route path='grafite' />
+                </Route>
+            </Routes>
+            <Footer />
+        </BrowserRouter>
+    )
+}
+
+export default RoutesApp;
+
